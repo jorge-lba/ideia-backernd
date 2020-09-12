b86d213afe3e7a6dc836171fbaecf04c9711b1a9
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-09-11 21:56:24 -0300

    Add - Middleware para verificar se o usuario e valido

--- app/Middleware/ConvertEmptyStringsToNull.js
+++ app/Middleware/ConvertEmptyStringsToNull.js
@@ -9,7 +9,6 @@ class ConvertEmptyStringsToNull {
         }))
       )
     }
-
     await next()
   }
 }
---
+++ app/Middleware/VerifyTokenFirebaseAuth.js
@@ -0,0 +1,36 @@
+'use strict'
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const firebase = use('Firebase/Admin')
+
+class VerifyTokenFirebaseAuth {
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Function} next
+   */
+  async handle ({ request, response }, next) {
+    try {  
+      const idToken = request.header('token')
+
+      if(idToken === undefined) return response.status(400).json({message: 'Token is undefined'})
+      
+      const token = await firebase
+        .auth()
+        .verifyIdToken(idToken.toString())
+
+      const date = parseInt(Date.now()/1000)
+
+      if( date > token.exp ) return response.status(400).json({message: 'Token expired'})
+      
+    } catch (error) {
+      console.log(error)
+    }
+        
+    await next()
+  }
+}
+
+module.exports = VerifyTokenFirebaseAuth
