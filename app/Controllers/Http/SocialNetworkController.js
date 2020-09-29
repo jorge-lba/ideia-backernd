7f5556e16b807ffa8710450bea180d27e830b8ae
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-09-28 23:12:55 -0300

    Change - Ajuste na variavel que recebe os dados para o cadastro da rede social

--- app/Controllers/Http/SocialNetworkController.js
+++ app/Controllers/Http/SocialNetworkController.js
@@ -21,7 +21,7 @@ class SocialNetworkController {
 
   async create ({ request, response }) {
     try {
-      const data = request.only(['socialNetworks'])
+      const { socialNetworks } = request.only(['socialNetworks'])
       const idToken = request.header('token')
 
       const token = await firebase
@@ -30,7 +30,7 @@ class SocialNetworkController {
 
       const uidAuth = token.uid
 
-      for (const socialNetwork of data) {
+      for (const socialNetwork of socialNetworks) {
         const { provider, url } = socialNetwork
         await SocialNetwork.create({
           uidUser: uidAuth,
@@ -38,6 +38,7 @@ class SocialNetworkController {
           url
         })
       }
+      console.log('Test Create')
 
       return response.status(200).json({
         status: 200,
