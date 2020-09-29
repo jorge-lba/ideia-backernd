0eeffb49cfe8c9bbb3db2057e793b7b226dec1e7
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-09-28 22:47:11 -0300

    Add - Controller para as redes sociais com o index e create

--- app/Controllers/Http/SocialNetworkController.js
+++ app/Controllers/Http/SocialNetworkController.js
@@ -1,12 +1,54 @@
 'use strict'
 
-const SocialNetwork = use('App/Model/SocialNetwork')
+const SocialNetwork = use('App/Models/SocialNetwork')
+const firebase = use('Firebase/Admin')
 
 class SocialNetworkController {
-  async index () {
-    const socialNetworks = await SocialNetwork.all()
+  async index ({ response }) {
+    try {
+      const socialNetworks = await SocialNetwork.all()
+      return response.status(200).json({
+        status: 200,
+        data: socialNetworks
+      })
+    } catch (error) {
+      return response.status(500).json({
+        status: 500,
+        error
+      })
+    }
+  }
 
-    return socialNetworks
+  async create ({ request, response }) {
+    try {
+      const data = request.only(['socialNetworks'])
+      const idToken = request.header('token')
+
+      const token = await firebase
+        .auth()
+        .verifyIdToken(idToken.toString())
+
+      const uidAuth = token.uid
+
+      for (const socialNetwork of data) {
+        const { provider, url } = socialNetwork
+        await SocialNetwork.create({
+          uidUser: uidAuth,
+          provider,
+          url
+        })
+      }
+
+      return response.status(200).json({
+        status: 200,
+        message: 'Redes adicionadas com sucesso!'
+      })
+    } catch (error) {
+      return response.status(500).json({
+        status: 500,
+        error
+      })
+    }
   }
 }
 
